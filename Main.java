7c8158bbc72ff99df880f31e87703ce04a8d2172
Aaron Hoffer <github@hoffer.org>
2016-09-08 11:15:29 -0700

    Experiments with Optional map and flatMap

---
+++ Main.java
@@ -0,0 +1,44 @@
+import java.util.Optional;
+
+public class Main  {
+
+    public static void main(String[] args) {
+        testReturnOptional(Optional.of("ALAKAZAM"), "an OPTIONAL WITH A VALUE");
+        println("(Flat map has extra smarts not to wrap an optional)");
+
+        testReturnOptional(Optional.empty(), "an empty optional");
+        println("(Map has enough smarts not to wrap an EMPTY optional)");
+
+        testReturnValue(Optional.ofNullable("BOJANGLES"), "an optional with a value");
+    }
+
+    private static void testReturnOptional(Optional<String> optional,
+            String descriptionOfOptional) {
+        String descriptionOfFunction = "a function that returns an OPTIONAL";
+        println("\nGIVEN " + descriptionOfOptional + " AND " + descriptionOfFunction + ", THEN");
+        Optional<?> outcomeOne = optional.flatMap(Main::myFunctionReturnsOptional);
+        Optional<?> outcomeTwo = optional.map(Main::myFunctionReturnsOptional);
+        println("\tMAP returns " + outcomeTwo);
+        println("\tFLAT MAP returns " + outcomeOne);
+    }
+
+    private static void testReturnValue(Optional<String> optional, String descriptionOfOptional) {
+        String descriptionOfFunction = "a function that returns an STRING";
+        println("\nGIVEN " + descriptionOfOptional + " AND " + descriptionOfFunction + ", THEN");
+        Optional<?> outcomeTwo = optional.map(Main::myFunctionReturnsString);
+        println("\tMAP returns " + outcomeTwo);
+        println("\tFLAT MAP does not accept functions that do not return an Optional.");
+    }
+
+    private static String myFunctionReturnsString(String value) {
+        return value;
+    }
+
+    private static Optional<String> myFunctionReturnsOptional(String s) {
+        return Optional.ofNullable(s);
+    }
+
+    static void println(Object o) {
+        System.out.println(o);
+    }
+}
